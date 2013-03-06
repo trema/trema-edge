edc52bccbfedd5e5ea74c867eea0d971ef8f1444
Nick Karanatsios <nickkaranatsios@gmail.com>
2013-03-06 11:54:25 +0900

    fix bug to be able to set multiple actions

--- src/switch/switch/action-helper.c
+++ src/switch/switch/action-helper.c
@@ -25,71 +25,93 @@
 action_list *
 _assign_actions( action_list *action_list, const struct ofp_action_header *action_hdr, uint16_t action_length ) {
   size_t offset = 0;
+  uint16_t type;
+
   while( action_length - offset >= sizeof( struct ofp_action_header ) ) {
-    if ( action_hdr->type == OFPAT_OUTPUT ) {
+    type = ( ( const struct ofp_action_header * )( ( const char * )action_hdr + offset ) )->type;
+    if ( type == OFPAT_OUTPUT ) {
       const struct ofp_action_output *action_output = ( const struct ofp_action_output * )( ( const char * )action_hdr + offset );
       action *ac = create_action_output( action_output->port, action_output->max_len );
       if ( ac != NULL ) {
+        offset += action_output->len;
         append_action( action_list, ac );
       }
     }
-    else if ( action_hdr->type == OFPAT_COPY_TTL_OUT ) {
+    else if ( type == OFPAT_COPY_TTL_OUT ) {
       append_action( action_list, create_action_copy_ttl_out() );
+      offset += sizeof( *action_hdr );
     }
-    else if ( action_hdr->type == OFPAT_COPY_TTL_IN ) {
+    else if ( type == OFPAT_COPY_TTL_IN ) {
       append_action( action_list, create_action_copy_ttl_in() );
+      offset += sizeof( *action_hdr );
     }
-    else if ( action_hdr->type == OFPAT_SET_MPLS_TTL ) {
+    else if ( type == OFPAT_SET_MPLS_TTL ) {
       const struct ofp_action_mpls_ttl *action_mpls_ttl = ( const struct ofp_action_mpls_ttl * )( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_set_mpls_ttl( action_mpls_ttl->mpls_ttl ) );
+      offset += action_mpls_ttl->len;
     }
-    else if ( action_hdr->type == OFPAT_DEC_MPLS_TTL ) {
+    else if ( type == OFPAT_DEC_MPLS_TTL ) {
       append_action( action_list, create_action_dec_mpls_ttl() );
+      offset += sizeof( *action_hdr );
     }
-    else if ( action_hdr->type == OFPAT_SET_NW_TTL ) {
+    else if ( type == OFPAT_SET_NW_TTL ) {
       const struct ofp_action_nw_ttl *action_nw_ttl = ( const struct ofp_action_nw_ttl *)( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_set_ipv4_ttl( action_nw_ttl->nw_ttl ) );
+      offset += action_nw_ttl->len;
     }
-    else if ( action_hdr->type == OFPAT_DEC_NW_TTL ) {
+    else if ( type == OFPAT_DEC_NW_TTL ) {
       append_action( action_list, create_action_dec_ipv4_ttl() );
+      offset += sizeof( *action_hdr );
     }
-    else if ( action_hdr->type == OFPAT_PUSH_VLAN ) {
+    else if ( type == OFPAT_PUSH_VLAN ) {
       const struct ofp_action_push *action_push = ( const struct ofp_action_push * )( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_push_vlan( action_push->ethertype ) );
+      offset += action_push->len;
     }
-    else if ( action_hdr->type == OFPAT_PUSH_MPLS ) {
+    else if ( type == OFPAT_PUSH_MPLS ) {
       const struct ofp_action_push *action_push = ( const struct ofp_action_push * )( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_push_mpls( action_push->ethertype ) );
+      offset += action_push->len;
     }
-    else if ( action_hdr->type == OFPAT_PUSH_PBB ) {
+    else if ( type == OFPAT_PUSH_PBB ) {
       const struct ofp_action_push *action_push = ( const struct ofp_action_push * )( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_push_pbb( action_push->ethertype ) );
+      offset += action_push->len;
     }
-    else if ( action_hdr->type == OFPAT_POP_VLAN ) {
+    else if ( type == OFPAT_POP_VLAN ) {
       append_action( action_list, create_action_pop_vlan() );
+      offset += sizeof( *action_hdr );
     }
-    else if ( action_hdr->type == OFPAT_POP_MPLS ) {
+    else if ( type == OFPAT_POP_MPLS ) {
       const struct ofp_action_pop_mpls *action_pop_mpls = ( const struct ofp_action_pop_mpls * )( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_pop_mpls( action_pop_mpls->ethertype ) );
+      offset += action_pop_mpls->len;
     }
-    else if ( action_hdr->type == OFPAT_POP_PBB ) {
+    else if ( type == OFPAT_POP_PBB ) {
       append_action( action_list, create_action_pop_pbb() );
+      offset += sizeof( *action_hdr );
     }
-    else if ( action_hdr->type == OFPAT_SET_QUEUE ) {
+    else if ( type == OFPAT_SET_QUEUE ) {
       const struct ofp_action_set_queue *set_queue = ( const struct ofp_action_set_queue * )( ( const char * )action_hdr + offset );
       append_action( action_list, create_action_set_queue( set_queue->queue_id ) );
+      offset += set_queue->len;
     }
-    else if ( action_hdr->type == OFPAT_SET_FIELD ) {
+    else if ( type == OFPAT_SET_FIELD ) {
       const struct ofp_action_set_field *set_field = ( const struct ofp_action_set_field * )( ( const char * )action_hdr + offset );
       match *match = create_match();
       assign_match( match, ( oxm_match_header * ) &set_field->field );
       append_action( action_list, create_action_set_field( match ) );
+      offset += set_field->len;
+    }
+    else if ( type == OFPAT_GROUP ) {
+      const struct ofp_action_group *action_group = ( const struct ofp_action_group * )( ( const char * )action_hdr + offset );
+      append_action( action_list, create_action_group( action_group->group_id ) );
+      offset += action_group->len;
     }
     else {
       warn( "Invalid action type %u", action_hdr->type );
       break;
     }
-    offset += action_hdr->len;
   }
   return action_list;
 }
