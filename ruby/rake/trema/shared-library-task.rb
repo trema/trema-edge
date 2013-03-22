d7f4b374fb11aac186233a34aa16210945940df3
Yasuhito Takamiya <yasuhito@gmail.com>
2013-03-22 15:32:46 +0900

    Refactor Rake::Trema::SharedLibraryTask.

--- ruby/rake/trema/shared-library-task.rb
+++ ruby/rake/trema/shared-library-task.rb
@@ -1,8 +1,29 @@
+#
+# Copyright (C) 2013 NEC Corporation
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
 require "rake/trema/library-task"
 
 
 module Rake
   module Trema
+    #
+    # Compile *.c files into a shared library.
+    #
     class SharedLibraryTask < LibraryTask
       attr_accessor :version
 
@@ -13,7 +34,6 @@ module Rake
 
 
       def generate_target
-        return if uptodate?( target_path, objects )
         sh "gcc -shared -Wl,-soname=#{ soname } -o #{ target_path } #{ objects.to_s }"
       end
 
